80d36144aa2484057599a30ca9013637516afba7
SirMathhman <sirmathhman@gmail.com>
2020-09-13 17:26:15 -0400

    Implement Compiler.compile for usage case

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -2,6 +2,9 @@ package com.meti;
 
 public class Compiler {
     String compile(String content) {
+        if(content.startsWith("def")) {
+            return "int main(){return 0;}";
+        }
         return content;
     }
 }
