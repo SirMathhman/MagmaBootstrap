49d53d52dec55d6c73fece902569a38828407c0d
SirMathhman <sirmathhman@gmail.com>
2020-09-13 17:37:34 -0400

    Complete FunctionTest

--- src/main/java/com/meti/Compiler.java
+++ src/main/java/com/meti/Compiler.java
@@ -2,20 +2,26 @@ package com.meti;
 
 public class Compiler {
     String compile(String content) {
-        if(content.startsWith("def")) {
+        if (content.startsWith("def")) {
             int paramStart = content.indexOf('(');
             String name = content.substring(4, paramStart).trim();
             int returnStart = content.indexOf(':');
             int returnEnd = content.indexOf("=>");
             String returnString = content.substring(returnStart + 1, returnEnd).trim();
-            String renderHeader;
-            if(returnString.equals("Int")) {
-                renderHeader = "int " + name;
-            } else {
-                renderHeader = "void " + name;
-            }
-            return renderHeader + "(){return 0;}";
+            String renderHeader = resolveString(returnString, name);
+            String value = content.substring(returnEnd + 2).trim();
+            return renderHeader + "()" + value;
         }
         return content;
+    }
+
+    private String resolveString(String value, String name) {
+        String renderHeader;
+        if (value.equals("Int")) {
+            renderHeader = "int " + name;
+        } else {
+            renderHeader = "void " + name;
+        }
+        return renderHeader;
     }
 }
