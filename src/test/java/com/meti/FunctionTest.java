1a3d89fb8a4e9e2d149e5f70a532ecc9c009e605
SirMathhman <sirmathhman@gmail.com>
2020-09-13 17:27:42 -0400

    Add more tests to FunctionTest

--- src/test/java/com/meti/FunctionTest.java
+++ src/test/java/com/meti/FunctionTest.java
@@ -6,9 +6,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FunctionTest {
     @Test
-    void compile() {
+    void testMain() {
         Compiler compiler = new Compiler();
         String target = compiler.compile("def main() : Int => {return 0;}");
         assertEquals("int main(){return 0;}", target);
+    }
+
+    @Test
+    void value(){
+        Compiler compiler = new Compiler();
+        String target = compiler.compile("def main() : Void => {return 0;}");
+        assertEquals("void main(){return 0;}", target);
+    }
+
+    @Test
+    void name(){
+        Compiler compiler = new Compiler();
+        String target = compiler.compile("def test : Void => {return 0;}");
+        assertEquals("void test(){return 0;}", target);
     }
 }
