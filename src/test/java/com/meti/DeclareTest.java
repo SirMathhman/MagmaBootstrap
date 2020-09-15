9e1c17bc5d6fb97243ec871f685a78cc84bd08c5
SirMathhman <sirmathhman@gmail.com>
2020-09-15 12:37:08 -0400

    Add test for declarations

---
+++ src/test/java/com/meti/DeclareTest.java
@@ -0,0 +1,10 @@
+package com.meti;
+
+import org.junit.jupiter.api.Test;
+
+public class DeclareTest extends CompileTest{
+    @Test
+    void define(){
+        assertCompile("int main(){int x=5;return 0;}", "def main() : Int => {const x = 5;return 0;}");
+    }
+}
