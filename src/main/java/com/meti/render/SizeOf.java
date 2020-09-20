be2e5272a91e9890b45cde01f9f36f3dae827db1
SirMathhman <sirmathhman@gmail.com>
2020-09-20 19:55:20 -0400

    Add better exception message to SizeOf

--- src/main/java/com/meti/render/SizeOf.java
+++ src/main/java/com/meti/render/SizeOf.java
@@ -36,7 +36,7 @@ public class SizeOf extends LeafNode {
 
     @Override
     public Optional<String> render() {
-        return Optional.of("sizeof(" + identity.render().orElseThrow() + ")");
+        return Optional.of("sizeof(" + identity.render().orElseThrow(() -> new IllegalStateException("Cannot render identity: " + identity)) + ")");
     }
 
     private static class PrototypeImpl implements Prototype {
