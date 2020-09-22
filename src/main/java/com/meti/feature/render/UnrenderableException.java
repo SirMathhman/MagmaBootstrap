bd5de5aed09c59b04a3574a2238a41b65f987621
SirMathhman <sirmathhman@gmail.com>
2020-09-22 14:13:22 -0400

    Introduce Renderable.render again
    This commit and the previous simply change the return type without breaking the code

---
+++ src/main/java/com/meti/feature/render/UnrenderableException.java
@@ -0,0 +1,9 @@
+package com.meti.feature.render;
+
+import com.meti.feature.CompileException;
+
+public class UnrenderableException extends CompileException {
+    public UnrenderableException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/meti/feature/scope/Renderable.java
+++ src/main/java/com/meti/feature/scope/Renderable.java
@@ -1,7 +1,13 @@
 package com.meti.feature.scope;
 
+import com.meti.feature.render.UnrenderableException;
+
 import java.util.Optional;
 
 public interface Renderable {
     Optional<String> renderOptionally();
+
+    default String render() {
+        return renderOptionally().orElseThrow(() -> new UnrenderableException("Not renderable."));
+    }
 }
