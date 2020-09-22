d04e102467cb42c72df1e7de5f716fb6a008bd86
SirMathhman <sirmathhman@gmail.com>
2020-09-22 15:25:09 -0400

    Pull up Int from outer class

---
+++ src/main/java/com/meti/feature/type/primitive/Int.java
@@ -0,0 +1,82 @@
+package com.meti.feature.type.primitive;
+
+import com.meti.content.Content;
+import com.meti.feature.render.Field;
+import com.meti.feature.render.Leaf;
+import com.meti.feature.render.Node;
+import com.meti.util.Monad;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+class Int extends Leaf {
+    private final int value;
+
+    public Int(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public <R> Optional<R> applyToContent(Function<Content, R> function) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Stream<Field> streamFields() {
+        return Stream.empty();
+    }
+
+    @Override
+    public Stream<Node> streamChildren() {
+        return Stream.empty();
+    }
+
+    @Override
+    public Prototype createPrototype() {
+        return new IntPrototype();
+    }
+
+    @Override
+    public Optional<String> renderOptionally() {
+        return Optional.of(String.valueOf(value));
+    }
+
+    @Override
+    public Monad<Group> group() {
+        return new Monad<>(Group.Integer);
+    }
+
+    private class IntPrototype implements Prototype {
+        @Override
+        public Prototype withField(Field field) {
+            return this;
+        }
+
+        @Override
+        public Prototype withChild(Node child) {
+            return this;
+        }
+
+        @Override
+        public Node build() {
+            return new Int(value);
+        }
+
+        @Override
+        public List<Node> listChildren() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public List<Field> listFields() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Prototype merge(Prototype other) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
--- src/main/java/com/meti/feature/type/primitive/IntTokenizer.java
+++ src/main/java/com/meti/feature/type/primitive/IntTokenizer.java
@@ -1,17 +1,11 @@
 package com.meti.feature.type.primitive;
 
 import com.meti.feature.evaluate.tokenize.AbstractNodeTokenizer;
-import com.meti.feature.render.Leaf;
-import com.meti.util.Monad;
 import com.meti.content.Content;
-import com.meti.feature.render.Field;
 import com.meti.feature.render.Node;
 
 import java.math.BigInteger;
-import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.stream.Stream;
 
 public class IntTokenizer extends AbstractNodeTokenizer {
     public static final BigInteger START = BigInteger.valueOf(-32767);
@@ -37,73 +31,4 @@ public class IntTokenizer extends AbstractNodeTokenizer {
         }
     }
 
-    private static class Int extends Leaf {
-        private final int value;
-
-        public Int(int value) {
-            this.value = value;
-        }
-
-        @Override
-        public <R> Optional<R> applyToContent(Function<Content, R> function) {
-            return Optional.empty();
-        }
-
-        @Override
-        public Stream<Field> streamFields() {
-            return Stream.empty();
-        }
-
-        @Override
-        public Stream<Node> streamChildren() {
-            return Stream.empty();
-        }
-
-        @Override
-        public Prototype createPrototype() {
-            return new IntPrototype();
-        }
-
-        @Override
-        public Optional<String> renderOptionally() {
-            return Optional.of(String.valueOf(value));
-        }
-
-        @Override
-        public Monad<Group> group(){
-            return new Monad<>(Group.Integer);
-        }
-
-        private class IntPrototype implements Prototype {
-            @Override
-            public Prototype withField(Field field) {
-                return this;
-            }
-
-            @Override
-            public Prototype withChild(Node child) {
-                return this;
-            }
-
-            @Override
-            public Node build() {
-                return new Int(value);
-            }
-
-            @Override
-            public List<Node> listChildren() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public List<Field> listFields() {
-                throw new UnsupportedOperationException();
-            }
-
-            @Override
-            public Prototype merge(Prototype other) {
-                throw new UnsupportedOperationException();
-            }
-        }
-    }
 }
