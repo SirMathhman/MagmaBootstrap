4f9c89547402df46a9f7364f649cc632bf281bb7
SirMathhman <sirmathhman@gmail.com>
2020-09-23 15:06:05 -0400

    Remove implementation of new keyword

--- src/main/java/com/meti/feature/evaluate/tokenize/MagmaNodeTokenizer.java
+++ src/main/java/com/meti/feature/evaluate/tokenize/MagmaNodeTokenizer.java
@@ -1,10 +1,16 @@
 package com.meti.feature.evaluate.tokenize;
 
 import com.meti.content.Content;
-import com.meti.feature.evaluate.Evaluator;
-import com.meti.feature.render.Node;
-import com.meti.feature.scope.NewTokenizer;
+import com.meti.feature.block.BlockTokenizer;
+import com.meti.feature.block.function.FunctionTokenizer;
 import com.meti.feature.block.function.ReturnTokenizer;
+import com.meti.feature.block.invoke.InvocationTokenizer;
+import com.meti.feature.evaluate.Evaluator;
+import com.meti.feature.extern.ImportTokenizerFactory;
+import com.meti.feature.render.Node;
+import com.meti.feature.scope.declare.DeclarationTokenizer;
+import com.meti.feature.scope.variable.VariableTokenizer;
+import com.meti.feature.type.CastTokenizer;
 import com.meti.feature.type.point.DereferenceTokenizer;
 import com.meti.feature.type.point.ReferenceTokenizer;
 import com.meti.feature.type.primitive.IntTokenizer;
@@ -12,13 +18,6 @@ import com.meti.feature.type.primitive.StringTokenizer;
 import com.meti.feature.type.structure.ConstructionTokenizer;
 import com.meti.feature.type.structure.FieldTokenizer;
 import com.meti.feature.type.structure.StructureTokenizer;
-import com.meti.feature.block.BlockTokenizer;
-import com.meti.feature.block.function.FunctionTokenizer;
-import com.meti.feature.block.invoke.InvocationTokenizer;
-import com.meti.feature.extern.ImportTokenizerFactory;
-import com.meti.feature.scope.declare.DeclarationTokenizer;
-import com.meti.feature.scope.variable.VariableTokenizer;
-import com.meti.feature.type.*;
 import com.meti.util.load.ClassPath;
 
 import java.util.function.Function;
@@ -37,7 +36,6 @@ public class MagmaNodeTokenizer extends CollectiveNodeTokenizer {
         return Stream.of(
                 factory::create,
                 StringTokenizer::new,
-                NewTokenizer::new,
                 CastTokenizer::new,
                 ConstructionTokenizer::new,
                 StructureTokenizer::new,
